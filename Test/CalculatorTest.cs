e5e0ad754474e8f28ba3054efd9a5dd3d30f8717
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-10-23 20:04:37 +0200

    update

---
+++ Test/CalculatorTest.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Threading.Tasks;
+using WeatherApi;
+using WeatherApi.Controllers;
+using Xunit;
+
+namespace Test
+{
+    public class CalculatorTest
+    {
+        private readonly Calculator _underTest = new Calculator();
+        
+        [Fact]
+        public void ItShouldAddTwoNumbers()
+        {
+            // arrange
+            const int numberOne = 1;
+            const int numberTwo = 2;
+            const int expected = 3;
+
+            // act
+            var result = _underTest.Add(numberOne, numberTwo);
+
+            // assert
+            Assert.Equal(result, expected);
+        }
+    }
+}
