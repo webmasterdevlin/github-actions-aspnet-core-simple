4807b328b1664de1afa51d7584773e7e947ae53d
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-10-23 21:24:50 +0200

    update

--- Test/CalculatorTest.cs
+++ Test/CalculatorTest.cs
@@ -9,14 +9,14 @@ namespace Test
     public class CalculatorTest
     {
         private readonly Calculator _underTest = new Calculator();
-        
+
         [Fact]
         public void ItShouldAddTwoNumbers()
         {
             // arrange
             const int numberOne = 1;
             const int numberTwo = 2;
-            const int expected = 3;
+            const int expected = 5;
 
             // act
             var result = _underTest.Add(numberOne, numberTwo);
